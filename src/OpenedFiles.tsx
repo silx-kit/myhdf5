932acacb202eb6a1798b3bb5c87d84d9a98d8e7e
Axel Bocciarelli <axelboc@gmail.com>
2022-09-16 09:05:17 +0200

    Refactor sidebar into single component

---
+++ src/OpenedFiles.tsx
@@ -0,0 +1,98 @@
+import type { IconType } from 'react-icons';
+import { FiMonitor, FiGlobe, FiDownload, FiTrash2 } from 'react-icons/fi';
+import {
+  createSearchParams,
+  Link,
+  useNavigate,
+  useSearchParams,
+} from 'react-router-dom';
+import { clear } from 'suspend-react';
+import shallow from 'zustand/shallow';
+
+import sidebarStyles from './Sidebar.module.css';
+import type { H5File } from './stores';
+import { FileService, useStore } from './stores';
+
+const ICONS: Record<FileService, IconType> = {
+  [FileService.Local]: FiMonitor,
+  [FileService.Url]: FiGlobe,
+};
+
+function OpenedFiles() {
+  const { opened, removeFileAt } = useStore(
+    ({ opened, removeFileAt }) => ({ opened, removeFileAt }),
+    shallow
+  );
+
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+  const fileUrl = searchParams.get('url');
+
+  function removeFile(file: H5File, index: number, isActive: boolean) {
+    if (isActive) {
+      // Select next or previous file, or navigate back to homepage
+      const nextIndex = index < opened.length - 1 ? index + 1 : index - 1;
+      const urlParam =
+        nextIndex >= 0 && createSearchParams({ url: opened[nextIndex].url });
+
+      navigate(urlParam ? `view?${urlParam.toString()}` : '/');
+    }
+
+    // Remove from store and evict from suspense cache
+    removeFileAt(index);
+    clear([file.url]);
+  }
+
+  return (
+    <>
+      <h2 className={sidebarStyles.heading}>Opened files</h2>
+      {opened.length > 0 ? (
+        opened.map((file, index) => {
+          const { name, url, service } = file;
+          const isActive = url === fileUrl;
+          const Icon = ICONS[service];
+
+          return (
+            <Link
+              key={url}
+              className={sidebarStyles.navItem}
+              to={`view?${createSearchParams({ url }).toString()}`}
+              title={url}
+              aria-current={isActive ? 'page' : undefined}
+            >
+              <Icon className={sidebarStyles.icon} />
+              <span className={sidebarStyles.label}>{name}</span>
+              <a
+                className={sidebarStyles.downloadBtn}
+                href={url}
+                download={name}
+                aria-label="Download file"
+                rel="noreferrer"
+                onClick={(evt) => {
+                  evt.stopPropagation();
+                }}
+              >
+                <FiDownload />
+              </a>
+              <button
+                className={sidebarStyles.removeBtn}
+                type="button"
+                aria-label="Remove file"
+                onClick={(evt) => {
+                  evt.preventDefault();
+                  removeFile(file, index, isActive);
+                }}
+              >
+                <FiTrash2 />
+              </button>
+            </Link>
+          );
+        })
+      ) : (
+        <p className={sidebarStyles.hint}>To get started, please open a file</p>
+      )}
+    </>
+  );
+}
+
+export default OpenedFiles;
--- src/Sidebar.tsx
+++ src/Sidebar.tsx
@@ -1,141 +1,76 @@
-import type { ReactNode } from 'react';
-import type { IconType } from 'react-icons';
-import {
-  FiDownload,
-  FiGlobe,
-  FiHelpCircle,
-  FiMonitor,
-  FiPlusCircle,
-  FiTrash2,
-} from 'react-icons/fi';
-import {
-  createSearchParams,
-  Link,
-  NavLink,
-  useNavigate,
-  useSearchParams,
-} from 'react-router-dom';
-import { clear } from 'suspend-react';
-import shallow from 'zustand/shallow';
+import { FiFileText, FiHelpCircle, FiPlusCircle } from 'react-icons/fi';
+import { NavLink, useMatch } from 'react-router-dom';
 
+import OpenedFiles from './OpenedFiles';
 import styles from './Sidebar.module.css';
-import type { H5File } from './stores';
-import { FileService } from './stores';
+import SidebarToggle from './SidebarToggle';
 import { useStore } from './stores';
 
-const ICONS: Record<FileService, IconType> = {
-  [FileService.Local]: FiMonitor,
-  [FileService.Url]: FiGlobe,
-};
-
-interface Props {
-  toggleBtn: ReactNode;
-}
-
-function Sidebar(props: Props) {
-  const { toggleBtn } = props;
-
-  const { opened, removeFileAt } = useStore(
-    ({ opened, removeFileAt }) => ({ opened, removeFileAt }),
-    shallow
-  );
-
-  const navigate = useNavigate();
-  const [searchParams] = useSearchParams();
-  const fileUrl = searchParams.get('url');
-
-  function removeFile(file: H5File, index: number, isActive: boolean) {
-    if (isActive) {
-      // Select next or previous file, or navigate back to homepage
-      const nextIndex = index < opened.length - 1 ? index + 1 : index - 1;
-      const urlParam =
-        nextIndex >= 0 && createSearchParams({ url: opened[nextIndex].url });
-
-      navigate(urlParam ? `view?${urlParam.toString()}` : '/');
-    }
-
-    // Remove from store and evict from suspense cache
-    removeFileAt(index);
-    clear([file.url]);
-  }
+function Sidebar() {
+  const isViewingFile = !!useMatch('/view');
+  const mayCollapse = useStore((state) => state.sidebarMayCollapse);
+  const isCollapsed = mayCollapse && isViewingFile;
 
   return (
-    <>
-      <h1 className={styles.title} data-reveal>
-        myHDF5
-      </h1>
-      <nav className={styles.nav} data-reveal>
-        <NavLink className={styles.navItem} to="/">
-          <FiPlusCircle className={styles.icon} />
-          <span className={styles.label}>Open HDF5</span>
-        </NavLink>
+    <div className={styles.sidebar} data-collapsed={isCollapsed || undefined}>
+      <div className={styles.sidebarInner}>
+        <h1 className={styles.title} data-reveal>
+          myHDF5
+        </h1>
+        <nav className={styles.nav} data-reveal>
+          <NavLink
+            className={styles.navItem}
+            to="/"
+            aria-label="Open HDF5"
+            title="Open HDF5"
+            data-primary
+          >
+            <FiPlusCircle className={styles.icon} />
+            <span className={styles.label}>Open HDF5</span>
+          </NavLink>
 
-        <NavLink className={styles.navItem} to="help">
-          <FiHelpCircle className={styles.icon} />
-          <span className={styles.label}>Help</span>
-        </NavLink>
+          <NavLink
+            className={styles.navItem}
+            to="help"
+            aria-label="Help"
+            title="Help"
+          >
+            <FiHelpCircle className={styles.icon} />
+            <span className={styles.label}>Help</span>
+          </NavLink>
 
-        <h2 className={styles.heading}>Opened files</h2>
-        {opened.length > 0 ? (
-          opened.map((file, index) => {
-            const { name, url, service } = file;
-            const isActive = url === fileUrl;
-            const Icon = ICONS[service];
+          {isCollapsed ? (
+            <button
+              type="button"
+              className={styles.navBtn}
+              aria-current={isViewingFile ? 'page' : undefined}
+              aria-label="Opened files"
+            >
+              <FiFileText />
+            </button>
+          ) : (
+            <OpenedFiles />
+          )}
+        </nav>
 
-            return (
-              <Link
-                key={url}
-                className={styles.navItem}
-                to={`view?${createSearchParams({ url }).toString()}`}
-                title={url}
-                aria-current={isActive ? 'page' : undefined}
-              >
-                <Icon className={styles.icon} />
-                <span className={styles.label}>{name}</span>
-                <a
-                  className={styles.downloadBtn}
-                  href={url}
-                  download={name}
-                  aria-label="Download file"
-                  rel="noreferrer"
-                  onClick={(evt) => {
-                    evt.stopPropagation();
-                  }}
-                >
-                  <FiDownload />
-                </a>
-                <button
-                  className={styles.removeBtn}
-                  type="button"
-                  aria-label="Remove file"
-                  onClick={(evt) => {
-                    evt.preventDefault();
-                    removeFile(file, index, isActive);
-                  }}
-                >
-                  <FiTrash2 />
-                </button>
-              </Link>
-            );
-          })
-        ) : (
-          <p className={styles.hint}>To get started, please open a file</p>
-        )}
-      </nav>
-      <div className={styles.footer}>
-        {toggleBtn}
-        <p className={styles.credits} data-reveal>
-          Made by{' '}
-          <a href="https://www.esrf.fr/" target="_blank" rel="noreferrer">
-            ESRF
-          </a>{' '}
-          with{' '}
-          <a href="https://www.panosc.eu/" target="_blank" rel="noreferrer">
-            european&nbsp;funding
-          </a>
-        </p>
+        <div className={styles.footer}>
+          <SidebarToggle
+            isCollapsed={isCollapsed}
+            isDisabled={!isViewingFile}
+          />
+          <p className={styles.credits} data-reveal>
+            Made by{' '}
+            <a href="https://www.esrf.fr/" target="_blank" rel="noreferrer">
+              ESRF
+            </a>{' '}
+            with{' '}
+            <a href="https://www.panosc.eu/" target="_blank" rel="noreferrer">
+              european&nbsp;funding
+            </a>
+          </p>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
 
