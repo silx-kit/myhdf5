dd92799126027f1fd25aadc310aba78c4423baeb
Axel Bocciarelli <axelboc@gmail.com>
2024-09-13 15:38:59 +0200

    Upgrade H5Web to v13

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import '@h5web/app/styles.css';
 import './index.css';
 
-import { assertNonNull } from '@h5web/app';
+import { assertNonNull, enableBigIntSerialization } from '@h5web/app';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
+
+enableBigIntSerialization();
 
 const rootElem = document.querySelector('#root');
 assertNonNull(rootElem);
